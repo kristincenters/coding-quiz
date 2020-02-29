853ab75c7984d94b0653acdecd4a0f382c6725d9
kcenters <kristincenters@gmail.com>
2020-02-29 16:33:43 -0600

    hard coded html

---
+++ script.js
@@ -0,0 +1,51 @@
+get
+
+
+var quizQuestions = [
+    {
+        question: 'Question1?',
+        multipleChoice: [
+            {
+                id: 'answer-a',
+                answer: 'a'
+            },
+            {
+                id: 'answer-b',
+                answer: 'b'
+            },
+            {
+                id: 'c',
+                answer: 'c'
+            },
+            {
+                id: 'd',
+                answer: "d"
+            }
+        ],
+        correct_answer: 'b'
+    },
+    {
+        question: 'question2?',
+        multipleChoice: [
+            {
+                id: 'a',
+                answer: 'The University of Utah'
+            },
+            {
+                id: 'b',
+                answer: 'Duke University'
+            },
+            {
+                id: 'c',
+                answer: 'Vanderbilt University'
+            },
+            {
+                id: 'd',
+                answer: 'The Penn State University'
+            }
+        ],
+        correct_answer: 'b'
+    }
+]
+
+if 
