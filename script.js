cb7f3f31841512df21fb15422107c8521ea11012
kcenters <kristincenters@gmail.com>
2020-02-29 19:19:37 -0600

    added html IDs and btn functionality

--- script.js
+++ script.js
@@ -1,51 +1,57 @@
-get
+//activate button
+var startBtn = document.querySelector('start');
+start.addEventListener('click', quizQuestions);
 
+let timer;
+
+function startQuiz() {
+	timer = setInterval(displayCounter, 1000);
+	//function displayQuestions();
+}
 
 var quizQuestions = [
-    {
-        question: 'Question1?',
-        multipleChoice: [
-            {
-                id: 'answer-a',
-                answer: 'a'
-            },
-            {
-                id: 'answer-b',
-                answer: 'b'
-            },
-            {
-                id: 'c',
-                answer: 'c'
-            },
-            {
-                id: 'd',
-                answer: "d"
-            }
-        ],
-        correct_answer: 'b'
-    },
-    {
-        question: 'question2?',
-        multipleChoice: [
-            {
-                id: 'a',
-                answer: 'The University of Utah'
-            },
-            {
-                id: 'b',
-                answer: 'Duke University'
-            },
-            {
-                id: 'c',
-                answer: 'Vanderbilt University'
-            },
-            {
-                id: 'd',
-                answer: 'The Penn State University'
-            }
-        ],
-        correct_answer: 'b'
-    }
-]
-
-if 
+	{
+		question: 'Question1?',
+		multipleChoice: [
+			{
+				id: 'answer-a',
+				answer: 'a'
+			},
+			{
+				id: 'answer-b',
+				answer: 'b'
+			},
+			{
+				id: 'answer-c',
+				answer: 'c'
+			},
+			{
+				id: 'answer-d',
+				answer: 'd'
+			}
+		],
+		correct_answer: 'b'
+	},
+	{
+		question: 'question2?',
+		multipleChoice: [
+			{
+				id: 'a',
+				answer: 'a'
+			},
+			{
+				id: 'b',
+				answer: 'b'
+			},
+			{
+				id: 'c',
+				answer: 'b'
+			},
+			{
+				id: 'd',
+				answer: 'b'
+			}
+		],
+		correct_answer: 'b'
+	}
+];
