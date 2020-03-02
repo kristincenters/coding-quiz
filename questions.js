01d7e7b343dea0c2999c1b010a3bd1a52dfb8cd5
kcenters <kristincenters@gmail.com>
2020-03-02 06:43:40 -0600

    starting over, new html

---
+++ questions.js
@@ -0,0 +1,17 @@
+var quizQuestions = [
+	{
+		question: 'Which statements are true?',
+		options: [
+			'JavaScript can be embedded within HTML',
+			'JavaScript is different from Java',
+			'JavaScript can be viewed in your browser window',
+			'All of the Above'
+		],
+		correctAnswer: '3'
+	},
+	{
+		question: 'Name a JavaScript Library',
+		options: ['React', 'Bootstrap', 'Sass', 'Ruby'],
+		correctAnswer: '0'
+	}
+];
--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 //load start button function
-window.onload = startButton;
+//window.onload = startButton;
 function startButton() {
-	alert('this button works');
-}
+alert('this button works');
+//}
+
+
 var startBtnElement = document.getElementById('startBtn');
 startBtnElement.addEventListener('click', startButton, false);
+console.log(startBtn);
+
+var questionsEl = document.getElementById('questions');
+var optionsEl = document.getElementsById('answers');
+var highscoreEl = document.getElementById(highscore);
+var renderCorrectAnswers = document.getElementById('correct');
+var timeClockEl = document.getElementById(countDown); 
 
 //const questions = document.querySelector('quizQuestions');
 //const timer = document.querySelector('timer');
@@ -17,28 +26,13 @@ setInterval(function() {
 		timer.innerText = 'Seconds Remaining:' + counter;
 	}
 }, 2000);
-
-function revealQuiz() {
-	event.preventDefault();
-	//timer = setInterval(displayCounter, 1000);
-	//randomQuestions = quizQuestions.shadowRoot(() => Math.random( - .5))
+if (counter < 0) {
+	counter = 0;
 }
 
-var quizQuestions = [
-	{
-		question: 'Which statements are true?',
-		answerA: '<JavaScript can be embedded within HTML>',
-		answerB: '<JavaScript is different from Java>',
-		answerC: '<JavaScript can be viewed in your browser window>',
-		answerD: '<All of the Above>',
-		correctAnswer: '3'
-	},
-	{
-		question: 'Name a JavaScript Library',
-		answerA: '<React>',
-		answerB: '<Bootstrap>',
-		answerC: '<Sass>',
-		answerD: '<Ruby',
-		correctAnswer: '0'
-	}
-];
+//function revealQuiz() {
+//	event.preventDefault();
+//	//timer = setInterval(displayCounter, 1000);
+//	//randomQuestions = quizQuestions.shadowRoot(() => Math.random( - .5))}
+
+var renderQuiz () 
