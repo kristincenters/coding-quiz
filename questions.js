1c761d4ceaea6dbd2234b12f7b5f26f0ca56a80b
kcenters <kristincenters@gmail.com>
2020-03-04 19:53:42 -0600

    working timer function

--- questions.js
+++ questions.js
@@ -1,17 +1,66 @@
-var quizQuestions = [
+window.onload = startButton;
+//activate button
+
+function startButton() {
+	//alert('this button works');
+}
+var startBtnElement = document.getElementById('startBtn');
+startBtnElement.addEventListener('click', startButton, false);
+
+//const questions = document.querySelector('quizQuestions');
+var timer = document.querySelector('timer');
+
+function timeClock() {
+	var counter = 20;
+	setInterval(function() {
+		if (counter >= 1) {
+			counter--;
+			var timer = document.getElementById('timer');
+			timer.innerText = 'Seconds Remaining:' + counter;
+		}
+	}, 2000);
+}
+function timeAlert() {
+	counter.textContent = 'Time Over';
+}
+
+/*var quizQuestions = [
 	{
-		question: 'Which statements are true?',
+		question: 'Guess the number of states where I've lived?',
 		options: [
-			'JavaScript can be embedded within HTML',
-			'JavaScript is different from Java',
-			'JavaScript can be viewed in your browser window',
-			'All of the Above'
+			'1',
+			'14',
+			'6',
+			'10'
 		],
-		correctAnswer: '3'
+		correctAnswer: '2'
 	},
 	{
-		question: 'Name a JavaScript Library',
-		options: ['React', 'Bootstrap', 'Sass', 'Ruby'],
-		correctAnswer: '0'
+		question: 'My favorite TV series?',
+		options: ['Mary Tyler Moore', 'Ozarks', 'The Handmaid's Tale', 'Mad Med'],
+		correctAnswer: '3'
 	}
 ];
+*/
+//var body = document.body
+//var renderQuestionDiv = document.getElementById('div');
+//var questionIndex = 0;
+/*
+var listChoices = document.getElementById('ul');
+options.forEach(function(options) {
+	let li = document.querySelector('li');
+	li.textContent = options;
+	listChoices.appendChild(li);
+});
+console.log(options);
+
+var options = document.querySelector(options);
+options.appendChild(listChoices);
+*/
+//var ChoiceBtn = document.createElement('btn');
+//var viewQuestion = renderQuestion(quizQuestions);
+//correctChoice = set.correctChoice;
+
+//var questionDiv = document.createElement(h2);
+//renderQuestion.innerText = set.question;
+//renderQuestion.appendChild(questionDiv);
--- script.js
+++ script.js
@@ -1,38 +1,59 @@
-//load start button function
-//window.onload = startButton;
+window.onload = startButton;
+//activate button
+
 function startButton() {
-alert('this button works');
-//}
-
-
-var startBtnElement = document.getElementById('startBtn');
-startBtnElement.addEventListener('click', startButton, false);
-console.log(startBtn);
-
-var questionsEl = document.getElementById('questions');
-var optionsEl = document.getElementsById('answers');
-var highscoreEl = document.getElementById(highscore);
-var renderCorrectAnswers = document.getElementById('correct');
-var timeClockEl = document.getElementById(countDown); 
-
-//const questions = document.querySelector('quizQuestions');
-//const timer = document.querySelector('timer');
-
-var counter = 20;
-setInterval(function() {
-	if (counter >= 1) {
-		counter--;
-		var timer = document.getElementById('timer');
-		timer.innerText = 'Seconds Remaining:' + counter;
-	}
-}, 2000);
-if (counter < 0) {
-	counter = 0;
+	//alert('this button works');
 }
 
-//function revealQuiz() {
-//	event.preventDefault();
-//	//timer = setInterval(displayCounter, 1000);
-//	//randomQuestions = quizQuestions.shadowRoot(() => Math.random( - .5))}
+var timerElement = document.getElementById('counter');
+var startBtnElement = document.getElementById('startBtn');
+startBtnElement.addEventListener('click', startButton, false);
 
-var renderQuiz () 
+var counter = 60;
+var timer = document.querySelector('counter');
+
+timeInterval = setInterval(function() {
+	if (counter >= 0) {
+		counter--;
+
+		timerElement.textContent = 'Remaining Seconds: ' + counter;
+		if (counter === 0) {
+			clearInterval(timeInterval);
+			alert('Game Over');
+		}
+	}
+}, 1000);
+
+var quizSection = document.getElementById('renderQuiz');
+//var questions = document.qetElementById('renderQuestion');
+var AnswerChoices = document.getElementById('renderOptions');
+var correctAnswer = document.getElementById('renderCorrect');
+var questionIndex = 0;
+var question;
+
+var quizQuestions = [
+	{
+		question: 'Guess the number of states where I have lived',
+		options: ['1', '14', '6', '10'],
+		correct: '2'
+	},
+	{
+		question: 'My favorite TV series',
+		options: ['Mary Tyler Moore', 'Ozarks', 'The Handmaids Tale', 'Mad Med'],
+		correct: '3'
+	}
+];
+renderQuiz.innerHTML = quizQuestions[0].question;
+//renderOptions.innerHTML = quizQuestions[0].options[i];
+
+function renderQA() {
+	for (var i = 0; 1 < quizQuestions[0].options.length; i++) {
+		var listChoices = document.getElementById('ul');
+		var li = document.querySelector('li');
+		console.log(listChoices);
+		li.innerHTML = question[0].options[i];
+		li.setAttribute('class', '#list');
+		listChoices.appendChild(li);
+	}
+}
+renderQA();
